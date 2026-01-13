f161bfce470a7f2db321b1ae999e91b5864b1c24
danpgomez <6013282+danpgomez@users.noreply.github.com>
2026-01-13 16:46:49 -0500

    Add cart icon to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
 import "./Header.css";
+import shoppingCartIcon from "url:./assets/shopping-cart-white.svg";
 
 function Header() {
   return (
     <header className="app_header">
       <h1 className="site_title">Daniel's Wine Shop</h1>
+      <img
+        src={shoppingCartIcon}
+        alt="shopping cart"
+        className="shopping-cart"
+      />
     </header>
   );
 }
