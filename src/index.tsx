a71cc8ffbb869b56d76be1d85f7e00d4307ae54a
danpgomez <6013282+danpgomez@users.noreply.github.com>
2026-01-22 12:38:28 -0500

    Standardize styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 import { App } from './App';
+import './index.css';
 
 let container = document.getElementById("app")!;
 let root = createRoot(container)
